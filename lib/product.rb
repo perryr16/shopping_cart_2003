47a8aa0981806a3d195209a5aba7b596be01c1a9
Ross Perry <perryr16@gmail.com>
2020-03-25 09:08:30 -0600

    iteration 1 complete

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,12 @@
-class Prodct
+class Product
 
+
+  attr_reader :category, :name, :unit_price, :quantity
   def initialize(category, name, unit_price, quantity)
     @category = category
-    @name = name_parameter
-    @unit_price = 3.70
-    @quantity = quantity
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
   end
 
   def total_price
