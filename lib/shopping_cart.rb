63245bcbd53ff8a18cb28285d33552b05be06401
Ross Perry <perryr16@gmail.com>
2020-03-25 09:09:48 -0600

    shopping cart class exists

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,10 @@
+class ShoppingCart
+
+
+  
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+  end
+
+end
---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+
+class ProductTest < Minitest::Test
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_instance_of ShoppingCart, cart
+
+  end
+
+end #final
