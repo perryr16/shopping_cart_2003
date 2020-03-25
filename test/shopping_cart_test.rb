c4ce17a41df4617b2a63c85589368398b2892814
Ross Perry <perryr16@gmail.com>
2020-03-25 09:11:05 -0600

    test it has a name

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -5,11 +5,16 @@ require './lib/shopping_cart'
 
 
 class ProductTest < Minitest::Test
+
   def test_it_exists
     cart = ShoppingCart.new("King Soopers", "30items")
-
     assert_instance_of ShoppingCart, cart
+  end
 
+  def test_it_has_a_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.name
   end
 
 end #final
